8c9bca8e445dcf70ad845ca54d2fbf107b4dad31
zed <90huanw@gmail.com>
2020-10-20 15:58:14 +0800

    sale

--- controllers/shipment.js
+++ controllers/shipment.js
@@ -1,5 +1,7 @@
 const product = require('../models/product');
 const ProductType = product.ProductType
+const Sale = product.Sale
+const Refund = product.Refund
 
 let getProductTypes = async (ctx) => {
     let page = ctx.query.page || 1
@@ -95,9 +97,83 @@ let deleteProductType = async (ctx) => {
     }
 };
 
+let getSales = async (ctx) => {
+    let page = ctx.query.page || 1
+    let limit = ctx.query.limit || 15
+    let searchKey = ctx.query.searchKey
+    let searchValue = ctx.query.searchValue
+    const search = {}
+    search[searchKey] = searchValue
+    let sales = []
+    let total = 0
+    if (!searchKey) {
+        sales = await Sale.findAll({ limit: limit, offset: (page - 1) * limit });
+        total = await Sale.count()
+    } else if (searchKey === "date") {
+        const dateList = searchValue.split(',')
+        sales = await Sale.findAll({
+            where: {
+                date: {
+                    [Op.between]: [dateList[0], dateList[1]]
+                }
+            },
+            limit: limit, offset: (page - 1) * limit
+        })
+        total = await Sale.count({
+            where: {
+                date: {
+                    [Op.between]: [dateList[0], dateList[1]]
+                }
+            }
+        })
+    } else {
+        sales = await Sale.findAll({ where: search, limit: limit, offset: (page - 1) * limit })
+        total = await Sale.count({ where: search })
+    }
+    ctx.body = {
+        code: 0,
+        msg: '获取成功！',
+        data: JSON.stringify(sales),
+        total: total
+    };
+};
+
+let createSale = async (ctx) => {
+    let data = ctx.request.body
+    await Sale.create(data)
+    ctx.body = {
+        code: 0,
+        msg: '创建成功！'
+    }
+};
+let updateSale = async (ctx) => {
+    let id = ctx.params.id
+    let data = ctx.request.body
+    let temp = await Sale.findByPk(id);
+    await temp.update(data)
+    ctx.body = {
+        code: 0,
+        msg: '修改成功！'
+    }
+};
+
+let deleteSale = async (ctx) => {
+    let id = ctx.params.id
+    let temp = await Sale.findByPk(id);
+    await temp.destroy()
+    ctx.body = {
+        code: 0,
+        msg: '删除成功！'
+    }
+};
+
 module.exports = {
     'GET /productTypes': getProductTypes,
     'POST /productTypes': createProductType,
     'PUT /productTypes/:id': updateProductType,
-    'DELETE /productTypes/:id': deleteProductType
+    'DELETE /productTypes/:id': deleteProductType,
+    'GET /sales': getSales,
+    'POST /sales': createSale,
+    'PUT /sales/:id': updateSale,
+    'DELETE /sales/:id': deleteSale
 };
--- models/product.js
+++ models/product.js
@@ -15,11 +15,93 @@ const ProductType = sequelize.define('ProductType', {
     tableName: 'product_types'
 });
 
+const Sale = sequelize.define('Sale', {
+  date: {
+    type: DataTypes.DATE
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  project: {
+    type: DataTypes.STRING
+  },
+  docnum: {
+    type: DataTypes.STRING
+  },
+  price: {
+    type: DataTypes.INTEGER
+  },
+  quantity: {
+    type: DataTypes.INTEGER
+  },
+  unit: {
+    type: DataTypes.STRING
+  },
+  amount: {
+    type: DataTypes.VIRTUAL,
+    get () {
+      return this.price * this.quantity
+    }
+  },
+  driver: {
+    type: DataTypes.STRING
+  },
+  remarks: {
+    type: DataTypes.TEXT
+  },
+}, {
+  tableName: 'sales',
+  timestamps: false
+});
+
+const Refund = sequelize.define('Refund', {
+  date: {
+    type: DataTypes.DATE
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  project: {
+    type: DataTypes.STRING
+  },
+  docnum: {
+    type: DataTypes.STRING
+  },
+  price: {
+    type: DataTypes.INTEGER
+  },
+  quantity: {
+    type: DataTypes.INTEGER
+  },
+  unit: {
+    type: DataTypes.STRING
+  },
+  amount: {
+    type: DataTypes.VIRTUAL,
+    get () {
+      return this.price * this.quantity
+    }
+  },
+  driver: {
+    type: DataTypes.STRING
+  },
+  remarks: {
+    type: DataTypes.TEXT
+  },
+}, {
+  tableName: 'refunds',
+  timestamps: false
+});
+
 // 初始化
 (async () => {
   await ProductType.sync({ force: false });
+  await Sale.sync({ force: false });
+  await Refund.sync({ force: false });
  })();
 
 module.exports = {
-  ProductType
+  ProductType,
+  Sale,
+  Refund
 };
