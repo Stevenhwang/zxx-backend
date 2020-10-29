58d58f9c78065585c1f32b68ffefea65870aa3eb
zed <90huanw@gmail.com>
2020-10-29 16:09:32 +0800

    add mysql

--- models/db.js
+++ models/db.js
@@ -1,8 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: './database.sqlite'
+// });
+
+const sequelize = new Sequelize('database', 'username', 'password',  {
+    host: 'localhost',
+    port:'3306',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
 });
 
 module.exports = {
